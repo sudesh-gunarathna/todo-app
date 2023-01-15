6cf07bbba2008cc420e0bce3b88f6b565708b603
sudesh Gunarathna <sudesh.jeewan@gmail.com>
2023-01-15 23:28:04 +0530

    change.

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -9,7 +9,7 @@ function TodoInput({todos, setTodos}) {
    const [todo, setTodo] = useState(""); 
 
 const handleClick =()=>{
-//id, name, isCompleated"
+//id, name, isCompleated" add as array object
 if(todo.length>0){
 const newTodo ={ id: v4(),
      name:todo, 
